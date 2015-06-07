a725d75ad37b8ed5b47f9d418a01dd1594a960ef
LPayyapilli <luke.payyapilli@gmail.com>
2015-06-07 18:47:01 -0400

    changed the serializer and the log to accept different ajax requests

--- app/serializers/picture_serializer.rb
+++ app/serializers/picture_serializer.rb
@@ -2,6 +2,6 @@ class PictureSerializer < ActiveModel::Serializer
   attributes :name, :url
 
   def url
-    "<img class='picture' data-id='#{object.id}' src ='https://s3-us-west-2.amazonaws.com/imagebucketeer/medium/#{object.id}/#{object.image_file_name}'/>"
+    "<img class='picture' data-id='#{object.id}' src ='https://s3-us-west-2.amazonaws.com/imagebucketeer/large/#{object.id}/#{object.image_file_name}'/>"
   end
 end
